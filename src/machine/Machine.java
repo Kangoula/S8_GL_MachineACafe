12d0a49d781957a4502bc3f7d150652c0c146b38
Guillaume DENIS <denisgme@gmail.com>
2016-01-28 17:36:36 +0100

    ajoute les méthodes de modification de la recette et du prix et les tests associés

--- src/machine/Machine.java
+++ src/machine/Machine.java
@@ -3,7 +3,7 @@ package machine;
 import java.util.HashMap;
 
 import machine.boisson.Boisson;
-import machine.caisse.Caisse;
+import machine.boisson.Recette;
 import machine.stock.Stock;
 
 /**
@@ -14,13 +14,15 @@ import machine.stock.Stock;
 public class Machine {
 
 	private HashMap<String, Boisson> listeBoissons;
+	private HashMap<String, Stock> listeIngrédients;
 	
 	public Machine() {
 		this.listeBoissons = new HashMap<String, Boisson>();
+		this.listeIngrédients = new HashMap<String, Stock>();
 	}
 	
 	/**
-	 * 
+	 * Ajoute une nouvelle boisson à la machine
 	 * @param boisson
 	 * @return la taille de la liste de boissons
 	 */
@@ -30,35 +32,53 @@ public class Machine {
 			this.listeBoissons.put(boisson.getNom(), boisson);	
 		}
 		
-		int size = this.listeBoissons.size();
-		
-		return size;
+		return this.listeBoissons.size();
 	}
 	
 	/**
-	 * 
+	 * Modifie la recette d'une boisson existante dans la machine
+	 * @param nomBoisson
+	 * @return la boisson modifiée
+	 */
+	public Boisson modifierRecetteBoisson(String nomBoisson, int qteCafe, int qteLait, int qteSucre, int qteChocolat){
+		Boisson b = this.listeBoissons.get(nomBoisson);
+		
+		if(b != null){
+			Recette r = new Recette(qteCafe, qteLait, qteSucre, qteChocolat);
+			b.setRecette(r);
+		}
+		
+		return b;
+	}
+	
+	/**
+	 * Modifie le prix d'une boisson existante dans la machine
+	 * @param nomBoisson
+	 * @param prix
+	 * @return la boisson modifée
+	 */
+	public Boisson modifierPrixBoisson(String nomBoisson, int prix){
+		Boisson b = this.listeBoissons.get(nomBoisson);
+		
+		if(b != null)
+			b.setPrix(prix);
+
+		return b;
+	}
+	
+	/**
+	 * Supprime une boisson de la machine
 	 * @param nomBoisson
 	 * @return
 	 */
-	public boolean modifierBoisson(String nomBoisson){
-		boolean res = false;
+	public int supprimerBoisson(String nomBoisson){
+		this.listeBoissons.remove(nomBoisson);
 		
-		return res;
+		return this.listeBoissons.size();
 	}
 	
 	/**
-	 * 
-	 * @param nomBoisson
-	 * @return
-	 */
-	public boolean supprimerBoisson(String nomBoisson){
-		boolean res = false;
-		
-		return res;
-	}
-	
-	/**
-	 * 
+	 * Ajoute un ingrédient au stock de la machine
 	 * @param nomIngredient
 	 * @param quantite
 	 * @return
--- src/machine/test/MachineTest.java
+++ src/machine/test/MachineTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import machine.Machine;
 import machine.boisson.Boisson;
+import machine.boisson.Recette;
 import machine.stock.Stock;
 
 public class MachineTest {
@@ -14,29 +15,59 @@ public class MachineTest {
 	public Machine m;
 	public Boisson b1, b2, b3, b4;
 	public Stock s1, s2, s3, s4;
+	public Recette r1, r2, r3, r4;
 	
 	@Before
 	public void setUp() throws Exception {
 		this.m = new Machine();
-		this.b1 = new Boisson(1, 4, 1, 0, "Café Au Lait", 3);
-		this.b2 = new Boisson(0,4,2,2,"Chocolat Chaud",2);
-		this.b3 = new Boisson(1, 4, 2, 1, "Capuccino", 4);
-		this.b4 = new Boisson(1, 1, 1, 1, "Je sais pas", 12);
+		
+		this.r1 = new Recette(1, 4, 1, 0);
+		this.r2 = new Recette(0, 4, 2, 2);
+		this.r3 = new Recette(1, 4, 2, 1);
+		this.r4 = new Recette(1, 2, 3, 4);
+		
+		this.b1 = new Boisson(r1, "Café Au Lait", 3);
+		this.b2 = new Boisson(r2, "Chocolat Chaud",2);
+		this.b3 = new Boisson(r3, "Capuccino", 4);
+		this.b4 = new Boisson(r4, "Je sais pas", 12);
+		
+		this.m.ajouterBoisson(b1);
+		this.m.ajouterBoisson(b2);
 	}
 
 	@Test
 	public void testAjoutBoisson() {
-		assertSame(this.m.ajouterBoisson(this.b1), 1);
+		assertSame(3, this.m.ajouterBoisson(this.b3));
 	}
 
 	@Test
-	public void testModifierBoisson() {
-		fail("Not yet implemented");
+	public void testModifierRecetteBoisson1() {
+		 assertEquals(4, this.m.modifierRecetteBoisson("Café Au Lait", 1, 2, 3, 4).getRecette().getQteChocolat());
+	}
+	
+	@Test
+	public void testModifierRecetteBoisson2() {
+		 assertNull(this.m.modifierRecetteBoisson("test", 1, 2, 3, 4));
+	}
+	
+	@Test
+	public void testModifierPrixBoisson1() {
+		 assertSame(1, this.m.modifierPrixBoisson("Café Au Lait", 1).getPrix());
 	}
 
 	@Test
-	public void testSupprimerBoisson() {
-		fail("Not yet implemented");
+	public void testModifierPrixBoisson2() {
+		 assertSame(1, this.m.modifierPrixBoisson("Café Au Lait", 1).getPrix());
+	}
+	
+	@Test
+	public void testSupprimerBoisson1() {
+		assertSame(1, this.m.supprimerBoisson("Café Au Lait"));
+	}
+	
+	@Test
+	public void testSupprimerBoisson2() {
+		assertSame(2, this.m.supprimerBoisson("yolo"));
 	}
 
 	@Test
