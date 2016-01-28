a15ab1e8f803abe741d7e54285a6f0bd09f372b2
Guillaume DENIS <denisgme@gmail.com>
2016-01-28 18:03:36 +0100

    ajout un début d'interface en ligne de commande

--- src/machine/Main.java
+++ src/machine/Main.java
@@ -1,9 +1,46 @@
 package machine;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// création de la machine
+		Machine m = new Machine();
+
+		Scanner sc = new Scanner(System.in);
+		String res;
+		String arrow = "->";
+		String menuAccueil = "1 - Demander une boisson\n2 - Gestion de la machine\n0 - Quitter :-(";
+		String menuCommande = "Veuillez sélectionner votre boisson :\n";
+		String menuGestion = "Que voulez-vous faire ?\n";
+		
+		System.out.println(menuAccueil);
+		System.out.print(arrow);
+
+		res = sc.next();
+
+		while (res != "0") {
+
+			switch (res) {
+			case "1":
+				System.out.println(menuCommande);
+				break;
+			case "2":
+				System.out.println(menuGestion);
+				break;
+			default:
+				System.out.println("Oups, mauvais numéro :/");
+				System.out.println(menuAccueil);
+				System.out.print(arrow);
+				break;
+			}
+			res = sc.next();
+		}
+
+		System.out.println("À bientôt !");
 
 	}
 
