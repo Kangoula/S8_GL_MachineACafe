278b2a90caf6f29fb5f73cbe481fc5b792db2359
Guillaume DENIS <denisgme@gmail.com>
2016-02-02 11:21:59 +0100

    Supprime le menu interactif

--- src/machine/Main.java
+++ src/machine/Main.java
@@ -1,47 +1,81 @@
 package machine;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
+
+import machine.boisson.Boisson;
+import machine.boisson.Recette;
+import machine.stock.Stock;
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		// création de la machine
-		Machine m = new Machine();
-
+		Machine m = initialiserMachine();
+		
+		//lancement de l'interface
 		Scanner sc = new Scanner(System.in);
 		String res;
-		String arrow = "->";
-		String menuAccueil = "1 - Demander une boisson\n2 - Gestion de la machine\n0 - Quitter :-(";
-		String menuCommande = "Veuillez sélectionner votre boisson :\n";
-		String menuGestion = "Que voulez-vous faire ?\n";
 		
-		System.out.println(menuAccueil);
-		System.out.print(arrow);
-
 		res = sc.next();
 
-		while (res != "0") {
+		while (!res.equals("0")) {
 
-			switch (res) {
-			case "1":
-				System.out.println(menuCommande);
-				break;
-			case "2":
-				System.out.println(menuGestion);
-				break;
-			default:
-				System.out.println("Oups, mauvais numéro :/");
-				System.out.println(menuAccueil);
-				System.out.print(arrow);
-				break;
-			}
+			
 			res = sc.next();
 		}
 
+		sc.close();
 		System.out.println("À bientôt !");
 
+	}
+
+	/**
+	 * Initialise la machine a café avec des valeurs par défaut
+	 * 
+	 * @return la machine initialisée
+	 */
+	public static Machine initialiserMachine() {
+		Machine m = new Machine();
+
+		Stock s1 = new Stock("café", 12);
+		Stock s2 = new Stock("LAIT", 50);
+		Stock s3 = new Stock("Chocolat", 14);
+		Stock s4 = new Stock("sucre", 43);
+
+		Recette r1 = new Recette();
+		Recette r2 = new Recette();
+		Recette r3 = new Recette();
+
+		r1.ajouterIngredient("Café", 2);
+		r1.ajouterIngredient("LAit", 2);
+		r1.ajouterIngredient("Chocolat", 0);
+		r1.ajouterIngredient("SUCRE", 1);
+
+		r2.ajouterIngredient("Café", 0);
+		r2.ajouterIngredient("LAit", 4);
+		r2.ajouterIngredient("Chocolat", 3);
+		r2.ajouterIngredient("SUCRE", 0);
+
+		r3.ajouterIngredient("Café", 1);
+		r3.ajouterIngredient("LAit", 4);
+		r3.ajouterIngredient("Chocolat", 1);
+		r3.ajouterIngredient("SUCRE", 1);
+
+		Boisson b1 = new Boisson(r1, "Café Au Lait", 3);
+		Boisson b2 = new Boisson(r2, "Chocolat Chaud", 2);
+		Boisson b3 = new Boisson(r3, "Capuccino", 4);
+
+		m.ajouterBoisson(b1);
+		m.ajouterBoisson(b2);
+		m.ajouterBoisson(b3);
+
+		m.ajouterStock(s1);
+		m.ajouterStock(s2);
+		m.ajouterStock(s3);
+		m.ajouterStock(s4);
+
+		return m;
 	}
 
 }
