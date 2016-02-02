a4c03fda802cec8c6e866a6abf70bc05d32cb982
Guillaume DENIS <denisgme@gmail.com>
2016-02-02 09:48:49 +0100

    ajoute les méthodes d'ajout et vérificaton des stocks d'ingrédients

--- src/machine/Machine.java
+++ src/machine/Machine.java
@@ -22,8 +22,8 @@ public class Machine {
 	}
 	
 	/**
-	 * Ajoute une nouvelle boisson à la machine
-	 * @param boisson
+	 * Ajoute une nouvelle boisson à la machine si la taille de la liste de boissons est plus petite que 3
+	 * @param boisson la boisson à ajouter à la machine
 	 * @return la taille de la liste de boissons
 	 */
 	public int ajouterBoisson(Boisson boisson){
@@ -37,11 +37,12 @@ public class Machine {
 	
 	/**
 	 * Modifie la recette d'une boisson existante dans la machine
-	 * @param nomBoisson
-	 * @return la boisson modifiée
+	 * @param nomBoisson le nom de la boisson dont on veut modifier la recette
+	 * @param nomIngredient le nom de l'ingredient à modifier ou ajouter à la recette
+	 * @param quantite la quantite de l'ingrédient à ajouter à la recette
+	 * @return la nouvelle recette de la boisson, null si la boisson n'est pas trouvée
 	 */
-	public Recette modifierRecetteBoisson(String nomBoisson, String nomIngredient, int quantite){
-		
+	public Recette modifierRecetteBoisson(String nomBoisson, String nomIngredient, int quantite){		
 		Recette r = null;
 		
 		if(this.listeBoissons.containsKey(nomBoisson)){
@@ -56,12 +57,12 @@ public class Machine {
 	
 	/**
 	 * Modifie le prix d'une boisson existante dans la machine
-	 * @param nomBoisson
-	 * @param prix
-	 * @return la boisson modifée
+	 * @param nomBoisson le nom de la boisson dont on veut modifier le prix
+	 * @param prix le nouveau prix de la boisson
+	 * @return la boisson modifée ou null si la boisson n'est pas trouvée
 	 */
 	public Boisson modifierPrixBoisson(String nomBoisson, int prix){
-		Boisson b = this.listeBoissons.get(nomBoisson);
+		Boisson b = this.listeBoissons.get(nomBoisson.toLowerCase());
 		
 		if(b != null)
 			b.setPrix(prix);
@@ -71,8 +72,8 @@ public class Machine {
 	
 	/**
 	 * Supprime une boisson de la machine
-	 * @param nomBoisson
-	 * @return
+	 * @param nomBoisson le nom de la boisson à supprimer
+	 * @return la taille de la liste de boissons de la machine
 	 */
 	public int supprimerBoisson(String nomBoisson){
 		this.listeBoissons.remove(nomBoisson.toLowerCase());
@@ -82,28 +83,40 @@ public class Machine {
 	
 	/**
 	 * Ajoute un ingrédient au stock de la machine
-	 * @param nomIngredient
-	 * @param quantite
-	 * @return
+	 * Si l'ingrédient existe, sa quantité est modifiée
+	 * S'il n'existe pas il est ajouté à la liste de ingrédients en sotck
+	 * @param stock le stock à ajouter à la machine
+	 * @return le stock ajouté à la liste, null si le stock n'a pas été ajouté
 	 */
-	public boolean ajoutIngredient(String nomIngredient, int quantite){
-		boolean res = false;
-				
-		return res;
+	public Stock ajouterStock(Stock stock){	
+		
+		if(this.stockIngredients.containsKey(stock.getType())){
+			this.stockIngredients.get(stock.getType()).augmenterStock(stock.getQuantite());
+		}
+		else {
+			this.stockIngredients.put(stock.getType(), stock);
+		}
+		
+		return this.stockIngredients.get(stock.getType());
 	}
 	
 	/**
-	 * 
-	 * @param nomIngredient
-	 * @return
+	 * Renvoit le Stock d'un ingréient donné
+	 * @param nomIngredient le nom de l'ingrédient dont on souhaite consulter le stock
+	 * @return la valeur actuelle du stock, -1 si le stock n'existe pas
 	 */
-	public String verifierStockIngredient(String nomIngredient){
-		String res = "";
+	public int verifierStockIngredient(String nomIngredient){
+		int res = -1;
+		String request = nomIngredient.toLowerCase();
+		
+		if(this.stockIngredients.containsKey(request)){
+			res = this.stockIngredients.get(request).getQuantite();
+		}
 		
 		return res;
 	}
 
-	
+	//GETTERS & SETTERS
 	public HashMap<String, Stock> getStockIngredients() {
 		return stockIngredients;
 	}
