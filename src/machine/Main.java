885712b34f00ae5ce3722229411a64a919383b66
Guillaume DENIS <denisgme@gmail.com>
2016-02-05 09:19:25 +0100

    ajoute l'ingrédient thé et deux boissons supplémentaires

--- src/machine/Main.java
+++ src/machine/Main.java
@@ -32,39 +32,64 @@ public class Main {
 		Stock s2 = new Stock("LAIT", 50);
 		Stock s3 = new Stock("Chocolat", 14);
 		Stock s4 = new Stock("sucre", 43);
+		Stock s5 = new Stock("thé", 50);
 
 		Recette r1 = new Recette();
 		Recette r2 = new Recette();
 		Recette r3 = new Recette();
+		Recette r4 = new Recette();
+		Recette r5 = new Recette();
 
 		r1.ajouterIngredient("Café", 2);
 		r1.ajouterIngredient("LAit", 2);
 		r1.ajouterIngredient("Chocolat", 0);
 		r1.ajouterIngredient("SUCRE", 1);
-
+		r1.ajouterIngredient("thé", 0);
+		
 		r2.ajouterIngredient("Café", 0);
 		r2.ajouterIngredient("LAit", 4);
 		r2.ajouterIngredient("Chocolat", 3);
 		r2.ajouterIngredient("SUCRE", 0);
+		r2.ajouterIngredient("thé", 0);
 
 		r3.ajouterIngredient("Café", 1);
 		r3.ajouterIngredient("LAit", 4);
 		r3.ajouterIngredient("Chocolat", 1);
 		r3.ajouterIngredient("SUCRE", 1);
+		r3.ajouterIngredient("thé", 0);
+		
+		r4.ajouterIngredient("thé", 2);
+		r4.ajouterIngredient("café", 0);
+		r4.ajouterIngredient("lait", 0);
+		r4.ajouterIngredient("chocolat", 0);
+		r4.ajouterIngredient("sucre", 1);
+		
+		r5.ajouterIngredient("thé", 2);
+		r5.ajouterIngredient("café", 0);
+		r5.ajouterIngredient("lait", 1);
+		r5.ajouterIngredient("chocolat", 0);
+		r5.ajouterIngredient("sucre", 1);
 
 		Boisson b1 = new Boisson(r1, "Café Au Lait", 3);
 		Boisson b2 = new Boisson(r2, "Chocolat Chaud", 2);
 		Boisson b3 = new Boisson(r3, "Capuccino", 4);
+		Boisson b4 = new Boisson(r4, "thé à la menthe", 2);
+		Boisson b5 = new Boisson(r5, "thé avec lait", 3);
 
 		m.ajouterBoisson(b1);
 		m.ajouterBoisson(b2);
 		m.ajouterBoisson(b3);
+		m.ajouterBoisson(b4);
+		m.ajouterBoisson(b5);
 
 		m.ajouterStock(s1);
 		m.ajouterStock(s2);
 		m.ajouterStock(s3);
 		m.ajouterStock(s4);
-
+		m.ajouterStock(s5);
+		
+		m.setNombreBoissonMax(5);
+		
 		return m;
 	}
 
