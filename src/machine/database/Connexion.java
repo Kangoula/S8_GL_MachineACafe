03a5317228ef0d24eda881ae2ab5bf4e7d488ac5
Antoine NOSAL <antoine.nosal@gmail.com>
2016-02-10 16:56:04 +0100

    ajoute la classe Connexion

---
+++ src/machine/database/Connexion.java
@@ -0,0 +1,40 @@
+package machine.database;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Classe connexion
+ * @author Antoine NOSAL
+ * @author Guillaume DENIS
+ */
+public class Connexion {
+    
+    private Connection c;
+    
+    /**
+     * Construit une connexion à la base de données.
+     */
+    public Connexion() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch (Exception e) {
+            // Problème
+        }
+    }
+    
+    /**
+     * Retroune la connexion à la base de données si elle existe,
+     * sinon elle en crée une puis la retourne
+     * @return 
+     */
+    public Connection getConnection() {
+        if(c==null) {
+            new Connexion();
+        }
+        return this.c;
+    }
+    
+}
