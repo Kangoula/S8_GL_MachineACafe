0db1c592e0446dc3f15c72fccdcc9c80f3e86e71
Guillaume DENIS <denisgme@gmail.com>
2016-02-15 21:28:50 +0100

    modifie la classe boisson pour le binding de haut niveau

--- src/fr/miage/machine/modele/boisson/Boisson.java
+++ src/fr/miage/machine/modele/boisson/Boisson.java
@@ -1,5 +1,12 @@
 package fr.miage.machine.modele.boisson;
 
+import javax.script.SimpleScriptContext;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Représente une boisson disponible dans la machine
  * @author Guillaume Denis
@@ -7,47 +14,53 @@ package fr.miage.machine.modele.boisson;
  */
 public class Boisson{
 
-	private String nom;
-	private int prix;
-	private Recette recette;
+	private final StringProperty name;
+	private final IntegerProperty price;
+	private Recette recipe;
 	
 	public Boisson(Recette recette, String nom, int prix) {
-		this.nom = nom;
-		this.prix = prix;
-		this.recette = recette;
+		this.name = new SimpleStringProperty(nom);
+		this.price = new SimpleIntegerProperty(prix);
+		this.recipe = recette;
 	}
 
 	//GETTERS & SETTERS
 	public Recette getRecette() {
-		return recette;
+		return recipe;
 	}
 
 	public void setRecette(Recette recette) {
-		this.recette = recette;
+		this.recipe = recette;
 	}
 
 	public void setNom(String nom) {
-		this.nom = nom;
+		this.name.set(nom);
 	}
 
 	public String getNom() {
-		return nom;
+		return name.get();
 	}
 
 	public int getPrix() {
-		return prix;
+		return price.get();
 	}
 
 	public void setPrix(int prix) {
-		this.prix = prix;
+		this.price.set(prix);
 	}
 
     @Override
     public String toString() {
-        String res = nom + " (" + prix + "€)";
+        String res = name.get() + " (" + price.get() + "€)";
         //res += "\n[Recette]\n"+recette;
         return res;
     }
 
-	
+	public StringProperty nameProperty() {
+		return this.name;
+	}
+
+	public IntegerProperty priceProperty(){
+		return this.price;
+	}
 }
